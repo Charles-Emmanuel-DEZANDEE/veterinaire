65c88e7cb3f50e11c994230019a9fee9667ca3a3
AD\cdezandee2017 <cdezandee@gmail.com>
2018-02-27 15:59:02 +0100

    gestion animal

---
+++ src/fr/eni/clinique/ihm/animal/AnimalController.java
@@ -0,0 +1,83 @@
+package fr.eni.clinique.ihm.log;
+
+import fr.eni.clinique.bll.BLLException;
+import fr.eni.clinique.bll.PersonnelsManager;
+import fr.eni.clinique.bo.Personnels;
+import fr.eni.clinique.dal.DALException;
+import fr.eni.clinique.ihm.acceuil.AcceuilController;
+
+public class LogInController {
+    private MDIAppLogIn fenetreLogIn;
+    private static LogInController instance;
+
+
+    private LogInController() throws DALException, BLLException {
+        fenetreLogIn = MDIAppLogIn.getInstance();
+    }
+
+    public static synchronized LogInController getInstance() throws DALException, BLLException {
+        if (instance == null) {
+            instance = new LogInController();
+        }
+        return instance;
+    }
+
+    public void startApp() throws BLLException, DALException {
+        fenetreLogIn.initLog();
+    }
+
+    public void validLogIn(String nom, String password) throws BLLException, DALException {
+
+
+        //on fait find by nom
+        Personnels user = PersonnelsManager.getInstance().getPersonnelByNom(nom);
+
+        if (user != null) {
+
+            //on récupére le mdp
+            String passwordBase = user.getMotPasse();
+
+            //on compare le mot de passe enregistrer avec la saisie
+
+            if (password.equals(passwordBase)) {
+                System.out.println("mot de pass bon");
+                //on lance une nouvelle fenetre suivant le role de l'user
+                String role = user.getRole();
+                switch (role) {
+                    case "adm":
+                        System.out.println("admin");
+                        AcceuilController.getInstance().start(3);
+                        break;
+                    case "sec":
+                        System.out.println("secretaire");
+                        AcceuilController.getInstance().start(1);
+                        break;
+                    case "vet":
+                        System.out.println("veterinaire");
+                        AcceuilController.getInstance().start(2);
+                        break;
+                    default:
+                        System.out.println("Pas de role existant");
+                }
+            }
+            //sinon on affiche l'erreur de pass
+
+            else {
+                fenetreLogIn.showError(2);
+                System.out.println("Probleme de pass");
+            }
+        }
+        //sinon on affiche l'erreur de user
+        else {
+            fenetreLogIn.showError(1);
+            System.out.println("Probleme de user");
+        }
+        //on ferme la fenetre de connexion
+        fenetreLogIn.exit();
+
+    }
+
+    public MDIAppLogIn getFenetreLogIn() {
+        return fenetreLogIn;
+    }
+}
---
+++ src/fr/eni/clinique/ihm/animal/MDIAppAnimal.java
@@ -0,0 +1,239 @@
+package fr.eni.clinique.ihm.log;
+
+import fr.eni.clinique.bll.BLLException;
+import fr.eni.clinique.dal.DALException;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class MDIAppLogIn extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+    private static MDIAppLogIn instance;
+
+    private InternalLogIn frmLog;
+
+	private JLabel labelLogNom;
+	private JLabel labelLogPassword;
+    private JLabel labelErrorUser;
+    private JLabel labelErrorPass;
+    private JTextField fieldLogNom;
+    private JTextField fieldLogPassword;
+    private JButton buttonLogValider;
+    private JButton buttonRetour;
+
+	//singleton
+    public static synchronized MDIAppLogIn getInstance() throws DALException, BLLException{
+        if (instance == null){
+            instance = new MDIAppLogIn();
+        }
+        return instance;
+    }
+
+
+
+    private MDIAppLogIn() {
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(500, 300);
+        setResizable(false);
+        setTitle("Connexion");
+        setVisible(true);
+
+	}
+	public void initLog() throws BLLException, DALException {
+
+        createLogPanel();
+        instance.revalidate();
+        instance.repaint();
+    }
+
+    public void showError (int cas){
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+
+        // Ligne 1
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        switch (cas) {
+            case 1:
+                panel.add(this.getLabelErrorUser(), gbc);
+
+                break;
+            case 2:
+                panel.add(this.getLabelErrorPass(), gbc);
+                break;
+
+            default:
+                System.out.println("Probleme erreur");
+        }
+
+        //ligne 2
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        panel.add(this.getButtonRetourn(), gbc);
+
+        setContentPane(panel);
+        instance.revalidate();
+        instance.repaint();
+
+    }
+
+	public void createLogPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+
+        // Ligne 1
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        panel.add(this.getLabelLogNom(), gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        panel.add(this.getFieldLogNom(), gbc);
+
+        // Ligne 2
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        panel.add(this.getLabelLogPassword(), gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        panel.add(this.getfieldLogPassword(), gbc);
+
+        //ligne 3
+        gbc.gridwidth = 2;
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        panel.add(this.getButtonLogValider(), gbc);
+
+        setContentPane(panel);
+    }
+
+
+	public void exit(){
+        setVisible(false);
+    }
+
+    public void reconnexion(){
+        String vide = "";
+        getFieldLogNom().setText(vide);
+        getfieldLogPassword().setText(vide);
+        setVisible(true);
+
+    }
+
+	public JLabel getLabelLogNom() {
+		if (this.labelLogNom == null) {
+			this.labelLogNom = new JLabel("Nom : ");
+			this.labelLogNom.setFont(new Font("Serif", Font.PLAIN, 20));
+		}
+		return this.labelLogNom;
+	}
+
+	public JLabel getLabelLogPassword() {
+		if (this.labelLogPassword == null) {
+			this.labelLogPassword = new JLabel("Mot de passe : ");
+			this.labelLogPassword.setFont(new Font("Serif", Font.PLAIN, 20));
+		}
+		return this.labelLogPassword;
+	}
+
+	public JLabel getLabelErrorUser() {
+		if (this.labelErrorUser == null) {
+			this.labelErrorUser = new JLabel("L'utilisateur est inconnu ");
+			this.labelErrorUser.setFont(new Font("Serif", Font.PLAIN, 20));
+		}
+		return this.labelErrorUser;
+	}
+
+	public JLabel getLabelErrorPass() {
+		if (this.labelErrorPass == null) {
+			this.labelErrorPass = new JLabel("Le mot de passe est faux ");
+			this.labelErrorPass.setFont(new Font("Serif", Font.PLAIN, 20));
+		}
+		return this.labelErrorPass;
+	}
+
+	public JTextField getFieldLogNom() {
+		if (this.fieldLogNom == null) {
+			this.fieldLogNom = new JTextField(20);
+		}
+		return this.fieldLogNom;
+	}
+
+	public JTextField getfieldLogPassword() {
+		if (this.fieldLogPassword == null) {
+			this.fieldLogPassword = new JTextField(20);
+		}
+		return this.fieldLogPassword;
+	}
+
+	public JButton getButtonLogValider() {
+		if (this.buttonLogValider == null) {
+			this.buttonLogValider = new JButton("Valider");
+//			this.buttonLogValider.setActionCommand("validerLogIn");
+            this.buttonLogValider.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    try {
+                        System.out.println("valider");
+                        //on envoi les saisies
+                        LogInController.getInstance().validLogIn(getFieldLogNom().getText(),getfieldLogPassword().getText());
+                    } catch (DALException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    } catch (BLLException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
+                }
+            });
+
+		}
+		return this.buttonLogValider;
+	}
+
+	public JButton getButtonRetourn() {
+		if (this.buttonRetour == null) {
+			this.buttonRetour = new JButton("Retour");
+//			this.buttonRetour.setActionCommand("validerLogIn");
+            this.buttonRetour.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    try {
+                        System.out.println("retour");
+                        //on remet la saisie
+                        initLog();
+                    } catch (DALException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    } catch (BLLException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
+                }
+            });
+
+		}
+		return this.buttonRetour;
+	}
+
+    public void setFieldLogNom(JTextField fieldLogNom) {
+        this.fieldLogNom = fieldLogNom;
+    }
+
+    public void setFieldLogPassword(JTextField fieldLogPassword) {
+        this.fieldLogPassword = fieldLogPassword;
+    }
+}
