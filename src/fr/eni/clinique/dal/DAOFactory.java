57016631f8ec3937bbfdbb2392c7cffe4320a1ee
AD\cdezandee2017 <cdezandee@gmail.com>
2018-02-28 14:44:29 +0100

    daofactory clients

--- src/fr/eni/clinique/dal/DAOFactory.java
+++ src/fr/eni/clinique/dal/DAOFactory.java
@@ -2,6 +2,7 @@ package fr.eni.clinique.dal;
 
 import fr.eni.clinique.bll.BLLException;
 import fr.eni.clinique.bo.Animaux;
+import fr.eni.clinique.bo.Clients;
 import fr.eni.clinique.bo.Personnels;
 
 public class DAOFactory {
@@ -17,18 +18,9 @@ public class DAOFactory {
 		return aDao;
 	}
 	
+	public static Dao<Clients> getClientsDAO() throws DALException, BLLException {
+		Dao<Clients> aDao = new ClientsDAOJdbcImpl();
+		return aDao;
+	}
 	
-	/*
-	public static Dao<Reservation> getReservationDAO() throws DALException, BLLException {
-		// todo mettre le jdbc de reservation
-		Dao<Reservation> rDao = new ReservationDAOJdbcImpl();
-		return rDao;
-	}
-
-	public static Dao<Client> getClientDAO() throws DALException, BLLException {
-		// todo mettre le jdbc de reservation
-		Dao<Client> cDao = new ClientDAOJdbcImpl();
-		return cDao;
-	}
-*/
 }
