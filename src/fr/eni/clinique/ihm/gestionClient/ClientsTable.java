ec58ac67d5af1fe2560ba8c42884b93e1f4190c5
AD\ekhadhar2017 <eddy-44600@live.fr>
2018-03-01 09:56:16 +0100

    no message

--- src/fr/eni/clinique/ihm/gestionClient/ClientsTable.java
+++ src/fr/eni/clinique/ihm/gestionClient/ClientsTable.java
@@ -3,6 +3,7 @@ package fr.eni.clinique.ihm.gestionClient;
 import javax.swing.JTable;
 
 import fr.eni.clinique.bll.BLLException;
+import fr.eni.clinique.bo.Clients;
 import fr.eni.clinique.dal.DALException;
 
 public class ClientsTable extends JTable{
