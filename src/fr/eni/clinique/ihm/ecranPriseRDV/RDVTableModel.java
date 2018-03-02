773f326886196209865ae50c8fe9c933d4314c7f
AD\amahmoudi2017 <azwaw.mahmoudi@gmail.com>
2018-03-02 17:23:52 +0100

    Fentre prise de RDV en cours

--- src/fr/eni/clinique/ihm/ecranPriseRDV/RDVTableModel.java
+++ src/fr/eni/clinique/ihm/ecranPriseRDV/RDVTableModel.java
@@ -4,49 +4,57 @@ import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
 
+import fr.eni.clinique.bll.AgendaManager;
 import fr.eni.clinique.bll.BLLException;
 import fr.eni.clinique.bll.PersonnelsManager;
 import fr.eni.clinique.bo.Personnels;
 import fr.eni.clinique.dal.DALException;
+import fr.eni.clinique.dto.RDV;
+import java.sql.Date;
+import java.util.Calendar;
 
 public class RDVTableModel extends AbstractTableModel{
 	
-	private List<Personnels> listePersonnel;
+	private List<RDV> listeRDV;
 	
-	public RDVTableModel() throws BLLException{
-		this.listePersonnel = PersonnelsManager.getInstance().getListePersonnel();
+	public RDVTableModel(int codeVet,Date dateRDV) throws BLLException{
+		try {
+			this.listeRDV = AgendaManager.getInstance().getRDVByVetEtDate(codeVet, dateRDV);
+		} catch (BLLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
-	public List<Personnels> getListePersonnel() {
-		return listePersonnel;
+	public List<RDV> getListePersonnel() {
+		return listeRDV;
 	}
 
 	@Override
 	public int getColumnCount() {
 		// TODO Auto-generated method stub
-		return 3;
+		return 4;
 	}
 
 	@Override
 	public int getRowCount() {
 		// TODO Auto-generated method stub
-		return this.listePersonnel.size();
+		return this.listeRDV.size();
 	}
 
 	@Override
 	public Object getValueAt(int arg0, int col) {
-		String motDePasse = "";
 		if (col == 0){
-			return listePersonnel.get(arg0).getNom();
+			return listeRDV.get(arg0).getDateRdv().getDate();
 		}
 		if(col == 1){
-			return listePersonnel.get(arg0).getRole();
+			return listeRDV.get(arg0).getNomClient();
 		}
-		if(col == 2){
-			for(int i=0; i<listePersonnel.get(arg0).getMotPasse().length();i++){
-				motDePasse += "*";
-			}
-			return motDePasse;
+		if(col == 1){
+			return listeRDV.get(arg0).getNomAnimal();
+		}
+		if(col == 1){
+			return listeRDV.get(arg0).getRace();
 		}
 		return null;
 	}
